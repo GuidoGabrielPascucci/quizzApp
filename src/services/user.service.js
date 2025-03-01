a9c82073bb3a17cbdde283c3db553e7447e97fcc
Guido Gabriel Pascucci <g.g.pascucci@gmail.com>
2025-02-28 21:46:49 -0300

    Modifico signup y agrego findByEmail en user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,7 +3,7 @@ import { User } from "../models/user.model.js";
 import { sanitizeUserForResponse, signToken } from "../utils/user.utils.js";
 
 export class UserService {
-
+    
     login = async (email, password) => {
         const user = await User.findOne({ email }).select("+password");
         if (!user || !await compare(password, user.password)) {
@@ -12,13 +12,20 @@ export class UserService {
         const accessToken = signToken(user);
         return accessToken;
     }
-
+    
     signup = async (user) => {
         const hashedPassword = await hash(user.password, 10);
-        user.password = hashedPassword;
-        const createdUser = await User.create(user);
+        const userWithHashedPassword = {
+            ...user,
+            password: hashedPassword
+        }
+        const createdUser = await User.create(userWithHashedPassword);
         const newUserDTO = sanitizeUserForResponse(createdUser);
         return newUserDTO;
     }
 
+    findByEmail = async (email) => {
+        const user = await User.findOne({email});
+        return user;
+    }
 }
