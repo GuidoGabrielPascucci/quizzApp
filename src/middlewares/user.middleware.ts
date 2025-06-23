d51a464724d1540ec7b307332d4ee04ebab7d769
Guido Gabriel Pascucci <g.g.pascucci@gmail.com>
2025-06-23 15:19:05 -0300

    Modifico middleware validateUpdateStatsInputs para que utilice validación con esquema valibot

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,7 +1,18 @@
 // LOGIN imports
 import { Request, Response, NextFunction } from "express";
 import { loginSchema } from "../schemas/login.schema.js";
-import { safeParse } from "valibot";
+//import { safeParse } from "valibot";
+import {
+    object,
+    string,
+    number,
+    date,
+    safeParse,
+    pipe,
+    nonEmpty,
+    minValue,
+    maxValue,
+} from "valibot";
 import { msg_mustEnterAllFieldsToLogin } from "./utils.middleware.js";
 // SIGNUP imports
 import { signupSchema } from "../schemas/signup.schema.js";
@@ -76,29 +87,64 @@ export function sanitizeSignupMw(
 }
 
 // UPDATE STATS
+// export function validateUpdateStatsInputs(
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ): void {
+//     const body: UserStatsNewData = req.body;
+//     if (
+//         !body.quizId ||
+//         !body.userId ||
+//         !body.category ||
+//         !body.score ||
+//         !body.totalQuestions ||
+//         !body.correctAnswers ||
+//         !body.completionTime ||
+//         !body.completedAt
+//     ) {
+//         res.status(400).json({
+//             success: false,
+//             message: "No has ingresado todos los campos",
+//         });
+//         return;
+//     }
+//     next();
+// }
+
+export const invalidDataResponseObject = {
+    success: false,
+    message: "Datos inválidos.",
+    //issues: result.issues, // Te da el detalle del error
+};
+
+const UserStatsSchema = object({
+    userId: pipe(string(), nonEmpty()),
+    quizId: pipe(string(), nonEmpty()),
+    category: pipe(string(), nonEmpty()),
+    score: pipe(number(), minValue(0), maxValue(500)),
+    correctAnswers: pipe(number(), minValue(0), maxValue(30)),
+    totalQuestions: pipe(number(), minValue(1), maxValue(30)),
+    completedAt: pipe(date(), minValue(new Date())),
+    completionTime: pipe(number(), minValue(1)),
+});
+
 export function validateUpdateStatsInputs(
     req: Request,
     res: Response,
     next: NextFunction
 ): void {
-    const body: UserStatsNewData = req.body;
-    if (
-        !body.quizId ||
-        !body.userId ||
-        !body.category ||
-        !body.score ||
-        !body.totalQuestions ||
-        !body.correctAnswers ||
-        !body.completionTime ||
-        !body.completedAt
-    ) {
-        res.status(400).json({
-            success: false,
-            message: "No has ingresado todos los campos",
-        });
+    const result = safeParse(UserStatsSchema, req.body);
+
+    // console.log(result);
+
+    if (!result.success) {
+        res.status(400).json(invalidDataResponseObject);
         return;
     }
+
+    // (opcional) reemplazar body con datos parseados y tipados
+    req.body = result.output;
+
     next();
 }
-
-
