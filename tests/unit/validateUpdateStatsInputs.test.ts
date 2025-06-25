cd129b18aeb1935c4ef0240123ad4d5e6cebd482
Guido Gabriel Pascucci <g.g.pascucci@gmail.com>
2025-06-25 11:18:28 -0300

    Mejoro lógica de test y reduzco redundancia de código

--- tests/unit/validateUpdateStatsInputs.test.ts
+++ tests/unit/validateUpdateStatsInputs.test.ts
@@ -22,16 +22,10 @@ describe("validateUpdateStatsInputs", () => {
     let body: Partial<UserStatsNewData> = {};
     const mockReq = (body: any): Request => ({ body } as Request);
     const mockRes = () => {
-        // 1. Creamos un objeto simple con las funciones mock que necesitamos.
         const res = {
-            // 2. Usamos .mockReturnThis() que está hecho exactamente para simular
-            //    métodos encadenables. Es un atajo para .mockReturnValue(this).
             status: jest.fn().mockReturnThis(),
             json: jest.fn().mockReturnThis(),
         };
-
-        // 3. Hacemos una aserción de tipo para que TypeScript trate nuestro
-        //    objeto simple como si fuera un objeto 'Response' completo.
         return res as unknown as Response;
     };
     const mockNext = jest.fn();
@@ -39,7 +33,6 @@ describe("validateUpdateStatsInputs", () => {
         jest.clearAllMocks();
         body = createRequestBody();
     });
-
     describe("Debería responder con status 400 si falta alguna propiedad", () => {
         const table = [
             "quizId",
@@ -61,44 +54,13 @@ describe("validateUpdateStatsInputs", () => {
             expect(mockNext).not.toHaveBeenCalled();
         });
     });
-
-    describe.skip("Deberia responder con 400 si alguna propiedad tiene un string vacío como valor", () => {
-        test("quizId es una cadena vacía", () => {
-            body.quizId = "";
-
+    describe("Deberia responder con status 400 si alguna propiedad tiene un string vacío como valor", () => {
+        const table = ["quizId", "userId", "category"];
+        test.each(table)("%s es una cadena vacía", (prop) => {
+            body = { ...body, [prop]: "" };
             const req = mockReq(body);
             const res = mockRes();
-
             validateUpdateStatsInputs(req as any, res, mockNext);
-
-            expect(mockNext).not.toHaveBeenCalled();
-            expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.json).toHaveBeenLastCalledWith(
-                invalidDataResponseObject
-            );
-        });
-        test("userId es una cadena vacía", () => {
-            body.userId = "";
-
-            const req = mockReq(body);
-            const res = mockRes();
-
-            validateUpdateStatsInputs(req as any, res, mockNext);
-
-            expect(mockNext).not.toHaveBeenCalled();
-            expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.json).toHaveBeenLastCalledWith(
-                invalidDataResponseObject
-            );
-        });
-        test("category es una cadena vacía", () => {
-            body.category = "";
-
-            const req = mockReq(body);
-            const res = mockRes();
-
-            validateUpdateStatsInputs(req as any, res, mockNext);
-
             expect(mockNext).not.toHaveBeenCalled();
             expect(res.status).toHaveBeenCalledWith(400);
             expect(res.json).toHaveBeenLastCalledWith(
@@ -106,37 +68,29 @@ describe("validateUpdateStatsInputs", () => {
             );
         });
     });
-    test.skip("debería responder con 400 si score es null", () => {
+    test("Debería responder con 400 si score es null", () => {
         body.score = null as any;
         const req = mockReq(body);
         const res = mockRes();
-
         validateUpdateStatsInputs(req as any, res, mockNext);
-
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith(invalidDataResponseObject);
         expect(mockNext).not.toHaveBeenCalled();
     });
-    test.skip("deberia responder con 400 si todos los datos están presentes pero alguno no respeta el tipo", () => {
+    test("Deberia responder con 400 si todos los datos están presentes pero alguno no respeta el tipo", () => {
         body.correctAnswers = "Dos" as any;
-
         const req = mockReq(body);
         const res = mockRes();
-
         validateUpdateStatsInputs(req as any, res, mockNext);
-
         expect(mockNext).not.toHaveBeenCalled();
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenLastCalledWith(invalidDataResponseObject);
     });
-    test.skip("debería llamar a next() si todos los datos están presentes", () => {
+    test("debería llamar a next() si todos los datos están presentes", () => {
         const req = mockReq(body);
         const res = mockRes();
-        // FUNCION TESTEADA
         validateUpdateStatsInputs(req as any, res, mockNext);
-        // El middleware llama a next()
         expect(mockNext).toHaveBeenCalled();
-        // Espera que res.status() no haya sido llamada
         expect(res.status).not.toHaveBeenCalled();
     });
 });
