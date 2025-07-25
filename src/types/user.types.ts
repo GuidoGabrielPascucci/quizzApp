29863c05c983db19d3b0678421c8be1ac553abe9
Guido Gabriel Pascucci <g.g.pascucci@gmail.com>
2025-07-25 14:56:00 -0300

    Agrega tipos de usuario y utilidades

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,3 +1,6 @@
+import { IUser } from "../interfaces/user.interface.js";
+// import { UserDocument } from "../models/user.model.js";
+
 export type UserSignupData = {
     firstname: string;
     lastname: string;
@@ -15,4 +18,14 @@ export type UserStatsNewData = {
     correctAnswers: number;
     completionTime: number;
     completedAt: Date;
+};
+
+export type UserDTO = Omit<IUser, "password" | "avatar"> & {
+    id: string;
+    avatar: string;
+};
+
+export type AuthResponse = {
+    accessToken: string;
+    user: UserDTO;
 };
--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -1,8 +1,10 @@
+import { UserDocument } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { UserDTO } from "../types/user.types.js";
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 
-export function sanitizeUserForResponse(createdUser: any) {
-    const createdUserPlainObj = createdUser.toObject();
+export function sanitizeUserForResponse(createdUser: UserDocument) {
+    const createdUserPlainObj = createdUser.toObject() as any;
     delete createdUserPlainObj.password;
     delete createdUserPlainObj.__v;
     return createdUserPlainObj;
@@ -33,3 +35,23 @@ export const invalidDataResponseObject = {
     success: false,
     message: "Datos inválidos.",
 };
+
+export const mapUserToResponse = (user: UserDocument): UserDTO => ({
+    id: user._id.toString(),
+    firstname: user.firstname,
+    lastname: user.lastname,
+    username: user.username,
+    email: user.email,
+    createdAt: user.createdAt,
+    avatar: user.avatar ?? "",
+    stats: user.stats ?? {
+        totalScore: 0,
+        totalCorrectAnswers: 0,
+        totalAnswersGiven: 0,
+        quizzesCompleted: 0,
+        highestScore: 0,
+        bestCategory: "",
+        categoryScores: {},
+    },
+    quizHistory: user.quizHistory,
+});
